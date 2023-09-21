69422c9280800d39c8fa9e41e0d1ce91f1c8686e
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-22 00:40:46 +0600

    about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -11,7 +11,7 @@ const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <div className="gradient-02 z-0" />
     <motion.div variants={staggerContainer} initial="hidden" whileInView="show" viewport={{once: false, amount: 0.25}} className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}>
-      <TypingText title="| About Metaversus" text={['About Me']} delay={3} />
+      <TypingText title="| About Metaversus" textStyles="text-center" />
     </motion.div>
   </section>
 );
