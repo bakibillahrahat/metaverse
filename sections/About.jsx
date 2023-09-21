023bac98323488120dcb6d09117df4ff0623b9e0
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-21 23:56:00 +0600

    about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,14 +1,15 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import { TypingText } from '../components';
 
 import styles from '../styles';
-import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+import { fadeIn, staggerContainer } from '../utils/motion';
 
 
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
   </section>
 );
 
