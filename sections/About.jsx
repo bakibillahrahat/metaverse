82491a9b7016b9215cd23e371f04c19481f9c71d
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-22 00:00:03 +0600

    about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -10,6 +10,9 @@ import { fadeIn, staggerContainer } from '../utils/motion';
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <div className="gradient-02 z-0" />
+    <motion.div variants={staggerContainer} initial="hidden" whileInView="show" viewport={{once: false, amount: 0.25}} className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}>
+      <TypingText title="| About Metaversus" text={['About Me']} delay={3} />
+    </motion.div>
   </section>
 );
 
