144ba4800dcc63b322e8becd0d03b57f95efa0f8
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-26 23:00:31 +0600

    fix some error

--- app/page.js
+++ app/page.js
@@ -21,12 +21,15 @@ const Page = () => (
     </div>
     <div className="relative">
       <GetStarted />
-      <div className="gradient-03 z-0" />
+      <div className="gradient-04 z-0" />
       <WhatsNew />
     </div>
     <World />
-    <Insights />
-    <Feedback />
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
+    </div>
     <Footer />
   </div>
 );
