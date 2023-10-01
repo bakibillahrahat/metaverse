1dad82bf78accc1a7a065fcece247d12240193aa
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-10-01 23:38:49 +0600

    add getstarted

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,5 +1,12 @@
 'use client';
 
+'use client';
+
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+
 const GetStarted = () => (
   <section>
     Get Started Section
