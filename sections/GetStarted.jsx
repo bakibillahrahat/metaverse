b17b5d6c62092122062b1ab902b13da11474c128
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-10-01 23:40:42 +0600

    import header

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -6,6 +6,8 @@ import { motion } from 'framer-motion';
 
 import styles from '../styles';
 import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { StartSteps, TitleText, TypingText} from '../components'
+import { startingFeatures } from '../constants'
 
 const GetStarted = () => (
   <section>
