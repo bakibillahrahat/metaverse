6eed27eed11afe892120eab9c74794330dfeebd0
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-10-03 23:23:21 +0600

    add insights section

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,4 +1,11 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { InsightCard, NewFeatures, TitleText, TypingText } from "../components";
+import { insights } from "../constants";
 
 const Insights = () => (
   <section>
