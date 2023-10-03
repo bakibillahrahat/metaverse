e5958db4de7ca89e12178e853d75b1e66a627358
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-10-03 22:51:30 +0600

    add world section

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,4 +1,10 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainer, fadeIn } from "../utils/motion";
+import { NewFeatures, TitleText, TypingText } from "../components";
 
 const World = () => (
   <section>
